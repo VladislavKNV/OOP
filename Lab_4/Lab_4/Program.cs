04ee4796ca13241de3743b6fbfaa02efa0321a27
Vlad <vladislav04082001@mail.ru>
2020-11-16 22:01:57 +0300

    4 labs

---
+++ Lab_4/Lab_4/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApplication1
+{
+    class Set
+    {
+        static void Main()
+        {
+            // Создадём два множества
+            SortedSet<char> ss = new SortedSet<char>();
+            SortedSet<char> ss1 = new SortedSet<char>();
+            SortedSet<char> ss2 = new SortedSet<char>();
+
+
+            ss.Add('A');
+            ss.Add('B');
+            ss.Add('C');
+            ss.Add('Z');
+            ShowColl(ss, "Первая коллекция: ");
+
+            ss1.Add('X');
+            ss1.Add('Y');
+            ss1.Add('Z');
+            ShowColl(ss1, "Вторая коллекция");
+
+            ss2.Add('A');
+            ss2.Add('B');
+            ss2.Add('B');
+            ss2.Add('C');
+            ShowColl(ss, "Первая коллекция: ");
+
+            IEnumerable<char> query = from planet in ss2.Distinct()
+                                        select planet;
+
+
+            ss.UnionWith(ss1);
+            ShowColl(ss, "Объединение множеств: ");
+
+            ss.ExceptWith(ss1);
+            ShowColl(ss, "Вычитание множеств");
+
+            Console.ReadLine();
+        }
+
+        static void ShowColl(SortedSet<char> ss, string s)
+        {
+            Console.WriteLine(s);
+            foreach (char ch in ss)
+                Console.Write(ch + " ");
+            Console.WriteLine("\n");
+        }
+    }
+}
